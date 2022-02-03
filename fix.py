ffb768e4db7f46682f5ff8507edadd9a824e6f83
Cyber_smile_54x45 <87195969+C7836-5M1L3@users.noreply.github.com>
2022-02-03 09:47:40 +0530

    Create fix.py

---
+++ fix.py
@@ -0,0 +1,21 @@
+
+
+
+  
+echo -e " \e[99m\e[1;96m
+------------------------------------------------------------------------------------------------
+
+  ___ _ __ _ __      / _(_)_  _____ _ __
+ / _ \ '__| '__|____| |_| \ \/ / _ \ '__|
+|  __/ |  | | |_____|  _| |>  <  __/ |
+ \___|_|  |_|       |_| |_/_/\_\___|_|                                                                                                   
+                                       coded by cyber smile
+-------------------------------------------------------------------------------------------------"
+sleep 3
+echo -e " \e [ERROR FIXED, TYPE pkg upgrade] \e[1;96
+  "
+
+
+echo "#" > /data/data/com.termux/files/usr/etc/apt/sources.list.d/science.list
+
+echo "#" > /data/data/com.termux/files/usr/etc/apt/sources.list.d/game.list
